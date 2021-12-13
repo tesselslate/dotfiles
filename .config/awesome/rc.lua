6b9b9cc24285cfc6f99a32cbd268502d220682f0
WoofWoofDoggo <woofwoofdoggo@protonmail.com>
2021-12-13 15:33:58 -0500

    add floating layout

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -45,7 +45,8 @@ awful.layout.layouts = {
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.top,
-    awful.layout.suit.max
+    awful.layout.suit.max,
+    awful.layout.suit.floating
 }
 
 -- keybinds
