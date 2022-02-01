6f5ecc4659fe2889c8867d57d2fae02f904dbf1f
woofdoggo <woofwoofdoggo@protonmail.com>
2022-02-01 16:50:01 -0500

    update awesomewm rules

--- .config/awesome/rules.lua
+++ .config/awesome/rules.lua
@@ -7,6 +7,29 @@ local beautiful = require("beautiful")
 
 local keys      = require("keys")
 
+local floating  = function(rule, custom_props)
+    local properties = {
+        focus           = awful.client.focus.filter,
+        raise           = true,
+        keys            = keys.client_keys,
+        buttons         = keys.client_buttons,
+        screen          = awful.screen.preferred,
+        placement       = awful.placement.no_overlap + awful.placement.no_offscreen,
+        floating        = true
+    }
+
+    if custom_props then
+        for k, v in pairs(custom_props) do
+            properties[k] = v
+        end
+    end
+
+    return {
+        rule = rule,
+        properties = properties
+    }
+end
+
 -- rules
 awful.rules.rules = {
     -- all clients
@@ -25,33 +48,17 @@ awful.rules.rules = {
     },
 
     -- celeste
-    {
-        rule = {
-            class = "Celeste.bin.x86_64"
-        },
-        properties = {
-            focus           = awful.client.focus.filter,
-            raise           = true,
-            keys            = keys.client_keys,
-            buttons         = keys.client_buttons,
-            screen          = awful.screen.preferred,
-            placement       = awful.placement.no_overlap + awful.placement.no_offscreen,
-            floating        = true
-        }
-    },
+    floating({
+        class = "Celeste.bin.x86_64"
+    }),
 
     -- minecraft
-    {
-        rule = {
-            class = "Minecraft* 1.16.1"
-        },
-        properties = {
-            focus           = awful.client.focus.filter,
-            raise           = true,
-            keys            = keys.client_keys,
-            buttons         = keys.client_buttons,
-            screen          = awful.screen.preferred,
-            floating        = true
-        }
-    }
+    floating({
+        class = "Minecraft* 1.16.1"
+    }),
+
+    -- obs projector
+    floating({
+        name = "Windowed Projector (Source) - Magnifier"
+    })
 }
