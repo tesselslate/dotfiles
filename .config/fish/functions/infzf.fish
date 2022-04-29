352b6a75de1bbcf1acf07d84c32c6855d1883b62
woofdoggo <woofwoofdoggo@protonmail.com>
2022-04-28 20:37:06 -0400

    update infzf fish function
    properly work when specifying search directory in last word

--- .config/fish/functions/infzf.fish
+++ .config/fish/functions/infzf.fish
@@ -1,10 +1,29 @@
 function infzf
     # get last word of commandline
-    set __last_word (string split ' ' (commandline -b))[-1]
+    set __buf (commandline -b)
+    set __words (string split ' ' $__buf)
+    set __last_word $__words[-1]
+
     if test (string length $__last_word) = 0
         set __last_word .
     end
 
-    # fzf
-    commandline -i (fd -H . $__last_word | fzf); commandline -f repaint
+    # get fzf result
+    set __fzfres (fd -H . $__last_word | fzf)
+
+    # if not successful, do nothing
+    if test $status -ne 0
+        commandline -f repaint
+        return
+    end
+
+    # set new commandline buffer
+    if test (count $__words) -gt 1
+        set __words $__words[1..(math (count $__words) - 1)]
+        set -a __words $__fzfres
+    else
+        set __words $__fzfres
+    end
+
+    commandline -r "$__words"; commandline -f repaint
 end
