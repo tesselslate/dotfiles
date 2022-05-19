814d403d0561acd1a729d44d711baee88c38ef0b
woofdoggo <woofwoofdoggo@protonmail.com>
2022-05-19 17:38:10 -0400

    remove useless commands

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -1,17 +1,7 @@
 -- neovim config
 -- commands.lua
 
--- Bterm - spawn a terminal at the bottom of the screen
-vim.cmd [[
-    command Bterm lua require("helpers/terminal").bottom_terminal()
-]]
-
 -- Rel - toggle relative line numbers
 vim.cmd [[ 
     command Rel if &relativenumber | set relativenumber& | else | set relativenumber | endif
-]]
-
--- Terminal - spawn floating terminal
-vim.cmd [[
-    command Terminal lua require("helpers/terminal").floating_terminal()
 ]]
