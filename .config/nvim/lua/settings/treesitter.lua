9131df3619a0314fd1bc64c1e6d757a17f06f731
woofdoggo <woofwoofdoggo@protonmail.com>
2022-06-14 20:53:40 -0400

    nvim: add tree-sitter-hare

--- .config/nvim/lua/settings/treesitter.lua
+++ .config/nvim/lua/settings/treesitter.lua
@@ -37,3 +37,15 @@ tsconfig.setup({
         additional_vim_regex_highlighting = false
     }
 })
+
+local parserconfig = require("nvim-treesitter.parsers").get_parser_configs()
+
+parserconfig.hare = {
+    install_info = {
+        url = "~/documents/clones/tree-sitter-hare",
+        files = {"src/parser.c"},
+        generate_requires_npm = false,
+        requires_generate_from_grammar = false,
+    },
+    filetype = "ha",
+}
