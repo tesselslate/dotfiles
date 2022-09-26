1198baf963e08f3c5a2b2e2408bfd6019b59da57
woofdoggo <woofwoofdoggo@protonmail.com>
2022-09-26 18:57:02 -0400

    colors: add catppuccin

---
+++ .config/desktop/colors/cat_macchiato/fish.fish
@@ -0,0 +1,26 @@
+# name: 'Catppuccin macchiato' 
+# url: 'https://github.com/catppuccin/fish'
+# preferred_background: 24273a
+
+set -g fish_color_normal cad3f5
+set -g fish_color_command 8aadf4
+set -g fish_color_param f0c6c6
+set -g fish_color_keyword ed8796
+set -g fish_color_quote a6da95
+set -g fish_color_redirection f5bde6
+set -g fish_color_end f5a97f
+set -g fish_color_error ed8796
+set -g fish_color_gray 6e738d
+set -g fish_color_selection --background=363a4f
+set -g fish_color_search_match --background=363a4f
+set -g fish_color_operator f5bde6
+set -g fish_color_escape f0c6c6
+set -g fish_color_autosuggestion 6e738d
+set -g fish_color_cancel ed8796
+set -g fish_color_cwd eed49f
+set -g fish_color_user 8bd5ca
+set -g fish_color_host 8aadf4
+set -g fish_pager_color_progress 6e738d
+set -g fish_pager_color_prefix f5bde6
+set -g fish_pager_color_completion cad3f5
+set -g fish_pager_color_description 6e738d
