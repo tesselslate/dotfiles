edfe4c5b03e1dd47e191a4965723f2ad5228cbd2
woofdoggo <46545045+woofdoggo@users.noreply.github.com>
2023-05-24 18:46:58 -0400

    wezterm: more cosmetic changes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,17 @@ local conf = wezterm.config_builder()
 conf.color_scheme = "tokyonight-storm"
 conf.default_cursor_style = "SteadyBar"
 conf.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+conf.hide_tab_bar_if_only_one_tab = true
+conf.tab_bar_at_bottom = true
+conf.tab_max_width = 10000
+conf.use_fancy_tab_bar = false
 conf.window_background_opacity = 0.85
 conf.window_frame = { font_size = 12 }
+conf.window_padding = {
+    left = "0.2cell",
+    right = "0.2cell",
+    top = "0.2cell",
+    bottom = 0,
+}
 
 return conf
