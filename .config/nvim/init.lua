4a8bc8ce85212dbbd5a98746e3033ca1de6f9ed9
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-05-02 20:38:19 -0400

    nvim: use mini.colors instead of tokyonight
    One less plugin and it boots up a bit faster. I don't know how often tokyonight updates but I haven't really noticed any changes or missing highlights.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,7 @@
 vim.loader.enable()
 
+require("tokyonight")
+
 --[[
     OPTIONS
 ]]--
@@ -186,37 +188,6 @@ require("lazy").setup({
                 use_icons = true,
                 set_vim_settings = false,
             })
-        end,
-    },
-    {
-        "folke/tokyonight.nvim",
-        config = function(_)
-            require("tokyonight").setup({
-                transparent = true,
-                terminal_colors = true,
-                styles = {
-                    comments = { italic = false },
-                    keywords = { italic = false },
-                },
-
-                on_colors = function(c)
-                    c.git = {
-                        add    = "#c3e88d",
-                        change = "#6fbcff",
-                        delete = "#ff757f",
-                    }
-                    c.gitSigns = c.git
-                end,
-                on_highlights = function(hl, c)
-                    hl.EndOfBuffer = {
-                        fg = nil,
-                    }
-                    hl.LineNr = {
-                        fg = c.fg_dark,
-                    }
-                end
-            })
-            vim.cmd("colorscheme tokyonight-moon")
         end,
     },
     {
