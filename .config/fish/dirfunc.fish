10f9ef19915ca109f2054facf3240414e160b02f
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-12-28 17:03:01 -0500

    fish: fix advent of code dirfunc

--- .config/fish/dirfunc.fish
+++ .config/fish/dirfunc.fish
@@ -1,11 +1,11 @@
 function __handle_dirfuncs --on-variable PWD
     if string match -q "$HOME/src/sandbox/comp/advent/2*" "$PWD"
         function r
-            python3 $HOME/src/sandbox/comp/advent/run.py
+            python3 $HOME/src/sandbox/comp/advent/run.py $argv
         end
 
         function g
-            python3 $HOME/src/sandbox/comp/advent/dl.py
+            python3 $HOME/src/sandbox/comp/advent/dl.py $argv
         end
     else
         functions --erase r
