e9700e08333c90bf62a5aca669f386601a6659c0
tesselslate <46545045+tesselslate@users.noreply.github.com>
2025-04-01 14:46:56 -0400

    fish: fix binds

--- .config/fish/binds.fish
+++ .config/fish/binds.fish
@@ -3,31 +3,31 @@
 # -----------------------------
 
 # ctrl-backspace => delete word
-bind -M insert  \cH backward-kill-word
+bind -M insert  ctrl-backspace  backward-kill-word
 
 # ctrl-e => edit command
-bind -M insert  \ce edit_command_buffer
+bind -M insert  ctrl-e          edit_command_buffer
 
 # ctrl-f => insert fzf result
-bind -M insert  \cf fzf_paste
+bind -M insert  ctrl-f          fzf_paste
 
 # ctrl-j => insert fzf history
-bind -M insert  \cj fzf_history
+bind -M insert  ctrl-j          fzf_history
 
 # ctrl-p => fzf and cd
-bind -M insert  \cp fzf_cd
+bind -M insert  ctrl-p          fzf_cd
 
 # ctrl-l => right arrow
-bind -M insert  \f forward-char
+bind -M insert  ctrl-l          forward-char
 
 # ctrl-o => open lf
-bind -M insert  \co lfcd
+bind -M insert  ctrl-o          lfcd
 
 # ctrl-x => normal mode
-bind -M insert  \cx 'set fish_bind_mode default; commandline -f repaint;'
+bind -M insert  ctrl-x          'set fish_bind_mode default; commandline -f repaint;'
 
 # unbind escape => normal mode
-bind -M insert  \e ''
+bind -M insert  escape          ''
 
 # -----------------------------
 # == normal mode
@@ -37,12 +37,3 @@ bind -M insert  \e ''
 bind -M default yy  fish_clipboard_copy
 bind -M default y   fish_clipboard_copy
 bind -M default p   fish_clipboard_paste
-
-# ctrl-j => insert fzf history
-bind -M insert  \cj fzf_history
-
-# ctrl-p => fzf and cd
-bind -M default \cp fzf_cd
-
-# ctrl-o => open lf
-bind -M default \cf lfcd
