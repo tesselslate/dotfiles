17f85e7e4f24fe5505a0ed4a0ac0a41389ba7e7b
tesselslate <46545045+tesselslate@users.noreply.github.com>
2025-06-04 19:17:17 -0400

    nvim: use new diagnostic signs config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -310,23 +310,22 @@ require("lazy").setup({
             vim.diagnostic.config({
                 update_in_insert = true, -- update diagnostics during insert mode
                 virtual_text     = false, -- no virtual text for diagnostics
+
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = "",
+                        [vim.diagnostic.severity.WARN]  = "",
+                        [vim.diagnostic.severity.HINT]  = "",
+                        [vim.diagnostic.severity.INFO]  = ""
+                    },
+                    numhl = {
+                        [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+                        [vim.diagnostic.severity.WARN]  = "DiagnosticSignWarn",
+                        [vim.diagnostic.severity.HINT]  = "DiagnosticSignHint",
+                        [vim.diagnostic.severity.INFO]  = "DiagnosticSignInfo",
+                    }
+                },
             })
-
-            local signs = {
-                Error = "",
-                Warn  = "",
-                Hint  = "",
-                Info  = ""
-            }
-
-            for type, icon in pairs(signs) do
-                local hl = "DiagnosticSign" .. type
-                vim.fn.sign_define(hl, {
-                    text = " " .. icon,
-                    texthl = hl,
-                    numhl = hl
-                })
-            end
 
             local lsp = require("lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
